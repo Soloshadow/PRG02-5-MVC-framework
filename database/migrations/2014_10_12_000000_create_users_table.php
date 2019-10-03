7290a39b6c8bc07f867a22845c6cfd1a5895faa0
Sonny <sonnylo@hotmail.com>
2019-10-03 22:58:43 +0200

    Updated db
    Updated the db, added the role model and inserted some data into db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreign('role_id')->references('id')->on('users_roles');
+            
+            $table->index("role_id");
         });
     }
 
--- database/migrations/2019_10_03_205157_create_roles_table.php
+++ database/migrations/2019_10_03_205157_create_roles_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreateUsersRolesTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_roles', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('role');
             $table->integer('level');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreateUsersRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_roles');
+        Schema::dropIfExists('roles');
     }
 }
