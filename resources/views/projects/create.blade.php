1b0c01d12d8377feec8c28efdda72cae6f214430
Sonny <sonnylo@hotmail.com>
2019-10-23 14:41:23 +0200

    Create project page
    Created a form page where project leaders can create projects for clients

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -6,14 +6,46 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Create a new project</div>
+                    <div class="card-header">{{ __('Create') }}</div>
+    
                     <div class="card-body">
-                        <form action="{{ route('home', ['user' => $user]) }}" method="post">
-                            {{-- Laravel method to protect against csrf --}}
+                        <form method="POST" action="{{ route('create') }}">
                             @csrf
-
+    
                             <div class="form-group row">
-                            <label for="project_name" class="col-md-4 col-form-label text-md-right">{{__('Project Name')}}</label>
+                                <label for="project_name" class="col-md-4 col-form-label text-md-right">{{ __('Project name') }}</label>
+    
+                                <div class="col-md-6">
+                                    <input id="project_name" type="text" class="form-control @error('project_name') is-invalid @enderror" name="project_name" value="{{ old('project_name') }}" required autocomplete="project_name" autofocus>
+    
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+    
+                            <div class="form-group row">
+                                <label for="company_name" class="col-md-4 col-form-label text-md-right">{{ __('Company name') }}</label>
+    
+                                <div class="col-md-6">
+                                    <input id="company_name" type="text" class="form-control @error('company_name') is-invalid @enderror" name="company_name" value="{{ old('company_name') }}" required autocomplete="company_name" autofocus>
+    
+                                    @error('company_name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Create') }}
+                                    </button>
+                                </div>
                             </div>
                         </form>
                     </div>
