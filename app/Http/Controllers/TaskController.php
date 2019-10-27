4cc7af28cb8884d22cd0f612c1eeff24de956540
Sonny <sonnylo@hotmail.com>
2019-10-27 12:03:29 +0100

    Creating TaskRequest
    Created a taskrequest for validating a form request in its own file

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,20 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\TaskRequest;
+
 Use App\Project;
 Use App\Task;
 
 class TaskController extends Controller
 {
     
-    public function store(Request $request,$user, $project){
+    public function store(TaskRequest $request,$user, $project){
 
-        // Form validation before storing in the DB
-        $validation = $request->validate([
-            'task' => 'required|min:5|max:255',
-            'progress' => 'required|filled', // user must choose a field
-            'moscow' => 'required|filled'
-        ]);
+        // Form validation before storing in the DB. This has been moved to the Taskrequest file
+        // $validation = $request->validate([
+        //     'task' => 'required|min:5|max:255',
+        //     'progress' => 'required|filled', // user must choose a field
+        //     'moscow' => 'required|filled'
+        // ]);
+
+        $validated = $request->validated();
 
         // get current project id to be linked to task
         $project_id = Project::find($project)->id;
